f2b1ffd5e61b2d5cc9b66d4faaf6a58241664afd
jnlong <zxl8310@163.com>
2017-04-17 16:11:39 +0800

    update

--- index.js
+++ index.js
@@ -4,11 +4,20 @@
  */
 
 var swig = require('swig');
-var confDef = {cache: false, varControls: ['{{', '}}'], tagControls: ['{%', '%}'], cmtControls: ['{#', '#}']};
+var fs = require('fs');
+var path = require('path');
+var confDef = { cache: false, varControls: ['{{', '}}'], tagControls: ['{%', '%}'], cmtControls: ['{#', '#}'] };
 
-module.exports = function(content,file,conf){
-	Object.assign(confDef, conf);
-	var template = swig.compileFile(file.realpath, confDef);
-	var output = template({});
-	return output;
+module.exports = function (content, file, conf) {
+    var isOut = conf.isOut;
+    Object.assign(confDef, conf);
+    conf = conf || {};
+    swig.setDefaults(confDef);
+    var html = swig.renderFile(file.toString());
+    if (isOut != undefined) {
+        fs.writeFileSync(file.realpathNoExt + '.html', html);
+        return content;
+    } else {
+        return html;
+    }
 }
